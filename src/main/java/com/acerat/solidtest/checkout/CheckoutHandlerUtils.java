355eb8e93d648cfef5854f797cb32c3139246a4b
Kevin Janvier Chinabalire <kevin.chinabalire@cellulant.com>
2019-10-23 14:27:35 +0300

    add Checkout Handler Utils

--- src/main/java/com/acerat/solidtest/checkout/CheckoutHandlerUtils.java
+++ src/main/java/com/acerat/solidtest/checkout/CheckoutHandlerUtils.java
@@ -1,4 +1,121 @@
 package com.acerat.solidtest.checkout;
 
-public class CheckoutHandlerUtils {
+import com.acerat.solidtest.cardpayments.CardDetails;
+import com.acerat.solidtest.cardpayments.CardPaymentService;
+import com.acerat.solidtest.checkout.state.*;
+import com.acerat.solidtest.configuration.ApplicationConfiguration;
+import com.acerat.solidtest.customers.Address;
+import com.acerat.solidtest.customers.Customer;
+import com.acerat.solidtest.customers.CustomerPaymentMethod;
+import com.acerat.solidtest.customers.CustomerRepository;
+import com.acerat.solidtest.encryptedstores.Encryption;
+import com.acerat.solidtest.encryptedstores.TrustStore;
+import com.acerat.solidtest.invoicing.InvoiceHandler;
+import com.acerat.solidtest.logistics.ShipmentTracker;
+import com.acerat.solidtest.shoppingcart.Order;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+class CheckoutHandlerUtils {
+
+    private static final CustomerRepository CUSTOMER_REPOSITORY =new CustomerRepository(ApplicationConfiguration.getConnectionString());
+private static final ShipmentTracker SHIPMENT_TRACKER = new ShipmentTracker(ApplicationConfiguration.getConnectionString());
+private static final CardPaymentService CARD_PAYMENT_SERVICE =new CardPaymentService(ApplicationConfiguration.getCardPaymentConfiguration());
+private static final InvoiceHandler INVOICE_HANDLER =new InvoiceHandler(ApplicationConfiguration.getConnectionString());
+
+public Customer getCustomer(Order order) {
+        // Get customer
+        return CUSTOMER_REPOSITORY.get(order.getCustomerId());
+    }
+
+    public boolean isValidShippingInformation(CheckoutState checkoutState, Customer customer) {
+        // Validate shipping information
+        if (getShippingAddress(customer) == null) {
+            shippingFailed(checkoutState, ShipmentFailures.MISSING_CUSTOMER_ADDRESS);
+            return false;
+        }
+        if (addressEmptyOrNull(getShippingAddress(customer))) {
+            shippingFailed(checkoutState, ShipmentFailures.INVALID_CUSTOMER_ADDRESS);
+            return false;
+        }
+        if (!SHIPMENT_TRACKER.canShipToDestination(getShippingAddress(customer))) {
+            shippingFailed(checkoutState, ShipmentFailures.CANNOT_SHIP_TO_DESTINATION);
+            return false;
+        }
+        checkoutState.shipmentVerified();
+        return true;
+    }
+
+    private void shippingFailed(CheckoutState checkoutState, ShipmentFailures invalidCustomerAddress) {
+        checkoutState.shipmentFailed(invalidCustomerAddress);
+    }
+
+    private Address getShippingAddress(Customer customer) {
+        return customer.getShippingAddress();
+    }
+
+
+
+    public boolean isChechoutState(CheckoutState checkoutState, Order order, Customer customer) {
+        // Make sure we don't charge customer twice
+        if (!checkoutState.isPaid()) {
+            // If the customer is set up to pay by card use the card payment service
+            if (customer.getConfiguration().getPaymentMenthod() == CustomerPaymentMethod.CARD) {
+                // Pick the currently valid credit card
+                // If there is no valid card update checkout state
+                if (!getCurrentCardDetails(customer).isPresent()) {
+                    checkoutState.cardPaymentFailed(CardPaymentFailures.NO_VALID_CREDIT_CARDS);
+                    return true;
+                }
+                if (CARD_PAYMENT_SERVICE.chargeCreditCard(getCurrentCardDetails(customer).get()).succeeded()) {
+                    checkoutState.cardPaymentCompletedUsing(getCurrentCardDetails(customer).get().getCardDetailsReference());
+                } else {
+                    checkoutState.cardPaymentFailed(CardPaymentFailures.COULD_NOT_COMPLETE_CARD_PAYMENT);
+                    return true;
+                }
+            } else if (customer.getConfiguration().getPaymentMenthod() == CustomerPaymentMethod.INVOICE) {
+                // Send invoice to customer
+                Address invoiceAddress = customer.getInvoiceAddress();
+                if (invoiceAddress == null) {
+                    checkoutState.failedToInvoiceCustomer(InvoiceFailures.MISSING_INVOICE_ADDRESS);
+                    return true;
+                }
+                if (addressEmptyOrNull(invoiceAddress)) {
+                    checkoutState.failedToInvoiceCustomer(InvoiceFailures.INVALID_CUSTOMER_ADDRESS);
+                    return true;
+                }
+                checkoutState.invoiceSentSuccessfully(INVOICE_HANDLER.produceInvoice(order, customer).getInvoiceId());
+            }
+        }
+        return false;
+    }
+
+    private Optional<CardDetails> getCurrentCardDetails(Customer customer) {
+        Optional<CardDetails> currentCardDetails = Optional.empty();
+        // Decrypt card details for our customer
+        List<CardDetails> cardDetailsList = decryptCustomerCard(customer);
+        for (CardDetails cardDetails : cardDetailsList) {
+            if (cardDetails.getExpiresAt().isAfter(LocalDate.now())) {
+                currentCardDetails = Optional.of(cardDetails);
+                break;
+            }
+        }
+        return currentCardDetails;
+    }
+
+    private List<CardDetails> decryptCustomerCard(Customer customer) {
+        TrustStore trustStore = new TrustStore(ApplicationConfiguration.getTrustStoreCredentials());
+        byte[] encryptedCardDetails = trustStore.getCardDetailsByCustomerId(customer.getCustomerId());
+        return Encryption.decryptFromSecret(encryptedCardDetails, customer.getCustomerSecret());
+    }
+
+    private boolean addressEmptyOrNull(Address shipmentAddress) {
+        return shipmentAddress.getStreet() == null || shipmentAddress.getStreet().isEmpty() ||
+                shipmentAddress.getZipCode() == null || shipmentAddress.getZipCode().isEmpty() ||
+                shipmentAddress.getCity() == null || shipmentAddress.getCity().isEmpty();
+    }
+
+
 }
--- src/main/java/com/acerat/solidtest/checkout/state/CheckoutState.java
+++ src/main/java/com/acerat/solidtest/checkout/state/CheckoutState.java
@@ -1,10 +1,17 @@
 package com.acerat.solidtest.checkout.state;
 
+import com.acerat.solidtest.configuration.ApplicationConfiguration;
+import com.acerat.solidtest.logistics.Warehouse;
+import com.acerat.solidtest.product.Product;
+import com.acerat.solidtest.product.ProductStore;
 import com.acerat.solidtest.shoppingcart.Order;
+import com.acerat.solidtest.shoppingcart.OrderLine;
 
 import java.util.UUID;
 
 public class CheckoutState {
+    private static final Warehouse WAREHOUSE = new Warehouse(ApplicationConfiguration.getConnectionString());
+    private static final ProductStore PRODUCT_STORE = new ProductStore(ApplicationConfiguration.getConnectionString());
     private Order order;
 
     public CheckoutState(Order order) {
@@ -47,5 +54,42 @@ public class CheckoutState {
     }
 
     public void shipmentActivated() {
+    }
+
+    public boolean sendReservedItems(Order order) {
+        for (OrderLine orderLine : order.getOrderLines()) {
+            Product product = PRODUCT_STORE.getById(orderLine.getProductId());
+            if (product == null) {
+                shipmentActivationFailed(WarehouseSendFailures.PRODUCT_NOT_FOUND);
+            }
+            if (!product.isStoredInWarehouse())
+                continue;
+            if (!WAREHOUSE.activateShipment(orderLine.getUniqueOrderLineReference())) {
+                shipmentActivationFailed(WarehouseSendFailures.COULD_NOT_ACTIVATE_SHIPMENT);
+                return true;
+            }
+        }
+        shipmentActivated();
+        return false;
+    }
+
+    public boolean getReverseItemsRelease(Order order) {
+        // Make sure we reserve items in stock in case they have been released
+        for (OrderLine orderLine : order.getOrderLines()) {
+            Product product = PRODUCT_STORE.getById(orderLine.getProductId());
+            if (product == null) {
+                warehouseReservationFailed(WarehouseReservationFailures.PRODUCT_NOT_FOUND);
+            }
+            if (!product.isStoredInWarehouse())
+                continue;
+            if (!WAREHOUSE.isReservedInStock(orderLine.getUniqueOrderLineReference(), orderLine.getQty())) {
+                if (!WAREHOUSE.tryReserveItems(orderLine.getUniqueOrderLineReference(), orderLine.getQty())) {
+                    warehouseReservationFailed(WarehouseReservationFailures.COULD_NOT_RESERVE_ITEMS_IN_STOCK);
+                    return true;
+                }
+            }
+        }
+        warehouseReservationSucceeded();
+        return false;
     }
 }
