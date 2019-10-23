81760579c4daaad13e2d108de3f547b7040d4741
Kevin Janvier Chinabalire <kevin.chinabalire@cellulant.com>
2019-10-23 17:08:35 +0300

    replace if with when

---
+++ README.md
@@ -0,0 +1,7 @@
+# Question
+           The objective is to refactor the legacy CheckoutHandler class. The end result should be a well-crafted code that expresses the domain and easy to read and maintain.
+
+>Refactor CheckoutHandler Class
+>Move sendReservedItems and getReverseItemsRelease inside CheckoutState to be clean
+
+[Java Branch](https://github.com/kevinjam/Refactoring-Exercise-by-acerat/tree/master)
--- src/main/java/com/acerat/solidtest/checkout/CheckoutHandler.kt
+++ src/main/java/com/acerat/solidtest/checkout/CheckoutHandler.kt
@@ -1,25 +1,32 @@
 package com.acerat.solidtest.checkout
 
+import com.acerat.solidtest.cardpayments.CardDetails
+import com.acerat.solidtest.cardpayments.CardPaymentService
 import com.acerat.solidtest.checkout.state.*
 import com.acerat.solidtest.configuration.ApplicationConfiguration
+import com.acerat.solidtest.customers.Address
 import com.acerat.solidtest.customers.Customer
+import com.acerat.solidtest.customers.CustomerPaymentMethod
 import com.acerat.solidtest.customers.CustomerRepository
+import com.acerat.solidtest.encryptedstores.Encryption
+import com.acerat.solidtest.encryptedstores.TrustStore
+import com.acerat.solidtest.invoicing.InvoiceHandler
+import com.acerat.solidtest.logistics.ShipmentTracker
 import com.acerat.solidtest.shoppingcart.Order
+import java.time.LocalDate
+import java.util.*
 
 class CheckoutHandler {
-    private val checkoutHandlerUtils = CheckoutHandlerUtils()
-
     fun checkout(checkoutState: CheckoutState): CheckoutState {
 
         val order = checkoutState.order
         val customer = getCustomer(order)
 
-        if (checkoutHandlerUtils.isValidShippingInformation(checkoutState, customer!!)) return checkoutState
-
+        if (isValidShippingInformation(checkoutState, customer!!)) return checkoutState
         //TODO Add logic
         if (checkoutState.getReverseItemsRelease(order)) return checkoutState
 
-        if (checkoutHandlerUtils.isCheckoutState(checkoutState, order, customer)) return checkoutState
+        if (isCheckoutState(checkoutState, order, customer)) return checkoutState
         // Send reserved items
         return if (checkoutState.sendReservedItems(order)) checkoutState else checkoutState
     }
@@ -28,14 +35,111 @@ class CheckoutHandler {
         return CheckoutState(null)
     }
 
-    fun getCustomer(order: Order): Customer? {
-        // Get customer
+    private fun getCustomer(order: Order): Customer? {
         return CUSTOMER_REPOSITORY.get(order.customerId)
     }
 
-    companion object {
-        private val CUSTOMER_REPOSITORY = CustomerRepository(ApplicationConfiguration.getConnectionString())
+    private fun isValidShippingInformation(checkoutState: CheckoutState, customer: Customer): Boolean {
+        // Validate shipping information
+
+        if (getShippingAddress(customer) == null) {
+            shippingFailed(checkoutState, ShipmentFailures.MISSING_CUSTOMER_ADDRESS)
+            return false
+        }
+        if (addressEmptyOrNull(getShippingAddress(customer)!!)) {
+            shippingFailed(checkoutState, ShipmentFailures.INVALID_CUSTOMER_ADDRESS)
+            return false
+        }
+        if (!SHIPMENT_TRACKER.canShipToDestination(getShippingAddress(customer))) {
+            shippingFailed(checkoutState, ShipmentFailures.CANNOT_SHIP_TO_DESTINATION)
+            return false
+        }
+        checkoutState.shipmentVerified()
+        return true
     }
+
+    private fun shippingFailed(checkoutState: CheckoutState, invalidCustomerAddress: ShipmentFailures) {
+        checkoutState.shipmentFailed(invalidCustomerAddress)
+    }
+
+    private fun getShippingAddress(customer: Customer): Address? {
+        return customer.shippingAddress
+    }
+
+
+    private fun isCheckoutState(checkoutState: CheckoutState, order: Order, customer: Customer): Boolean {
+        // Make sure we don't charge customer twice
+        when {
+            !checkoutState.isPaid -> // If the customer is set up to pay by card use the card payment service
+                when {
+                    customer.configuration!!.paymentMenthod == CustomerPaymentMethod.CARD -> // Pick the currently valid credit card
+                        // If there is no valid card update checkout state
+                        when {
+                            !getCurrentCardDetails(customer).isPresent -> {
+                                checkoutState.cardPaymentFailed(CardPaymentFailures.NO_VALID_CREDIT_CARDS)
+                                return false
+                            }
+                            CARD_PAYMENT_SERVICE.chargeCreditCard(getCurrentCardDetails(customer).get())!!.succeeded() -> checkoutState.cardPaymentCompletedUsing(getCurrentCardDetails(customer).get().cardDetailsReference)
+                            else -> {
+                                checkoutState.cardPaymentFailed(CardPaymentFailures.COULD_NOT_COMPLETE_CARD_PAYMENT)
+                                return false
+                            }
+                        }
+                    customer.configuration!!.paymentMenthod == CustomerPaymentMethod.INVOICE -> {
+                        // Send invoice to customer
+                        val invoiceAddress = customer.invoiceAddress
+                        when {
+                            invoiceAddress == null -> {
+                                checkoutState.failedToInvoiceCustomer(InvoiceFailures.MISSING_INVOICE_ADDRESS)
+                                return false
+                            }
+                            addressEmptyOrNull(invoiceAddress) -> {
+                                checkoutState.failedToInvoiceCustomer(InvoiceFailures.INVALID_CUSTOMER_ADDRESS)
+                                return false
+                            }
+                            else -> checkoutState.invoiceSentSuccessfully(INVOICE_HANDLER.produceInvoice(order, customer)!!.invoiceId)
+                        }
+                    }
+                }
+        }
+        return true
+    }
+
+    private fun getCurrentCardDetails(customer: Customer): Optional<CardDetails> {
+        var currentCardDetails = Optional.empty<CardDetails>()
+        // Decrypt card details for our customer
+        val cardDetailsList = decryptCustomerCard(customer)
+        for (cardDetails in cardDetailsList) {
+            if (cardDetails.expiresAt.isAfter(LocalDate.now())) {
+                currentCardDetails = Optional.of(cardDetails)
+                break
+            }
+        }
+        return currentCardDetails
+    }
+
+    private fun decryptCustomerCard(customer: Customer): List<CardDetails> {
+        return Encryption.decryptFromSecret(getCardDetailsByCustomerId(customer), customer.customerSecret)
+    }
+
+    private fun getCardDetailsByCustomerId(customer: Customer): ByteArray {
+        return TrustStore(ApplicationConfiguration.getTrustStoreCredentials()).getCardDetailsByCustomerId(customer.customerId)
+    }
+
+    private fun addressEmptyOrNull(shipmentAddress: Address): Boolean {
+        return shipmentAddress.street == null || shipmentAddress.street!!.isEmpty() ||
+                shipmentAddress.zipCode == null || shipmentAddress.zipCode!!.isEmpty() ||
+                shipmentAddress.city == null || shipmentAddress.city!!.isEmpty()
+    }
+
+    companion object {
+        private val SHIPMENT_TRACKER = ShipmentTracker(ApplicationConfiguration.getConnectionString())
+        private val CARD_PAYMENT_SERVICE = CardPaymentService(ApplicationConfiguration.getCardPaymentConfiguration())
+        private val INVOICE_HANDLER = InvoiceHandler(ApplicationConfiguration.getConnectionString())
+        private val CUSTOMER_REPOSITORY = CustomerRepository(ApplicationConfiguration.getConnectionString())
+
+    }
+
 
 
 }
